e74de169b786b073a64ab6d1abd40f71bf9a12f7
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-04-16 16:30:10 -0400

    updated some JS and HTML

--- jss.js
+++ jss.js
@@ -1,6 +1,6 @@
-
 var cuisine = $(this).attr("cuisine");
-var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query="  +cuisine
+var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=10&tags=french"
+var BaseURL = "https://spoonacular.com/recipeImages/"
 $.ajax({
     url: queryURL,
     method: "GET",
@@ -8,9 +8,32 @@ $.ajax({
         "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
         "X-RapidAPI-Key": "a116ebaf59msh44d46898d8f1b2ap1ffb3cjsn1d13ec828c85"
     }
-}).then(function (response) {
-    console.log(response);
-})
+}).then(function(response) {
+        console.log(response.recipes)
+        $("#container").empty()
+        
+        var recipe = response.recipes;
+        for (var i=0; i < recipe.length; i++) {
+            for (b = 0; b < recipe.length; b++){
+                var container = $("<container>")
+                var row;
+                if (b % 3 === 0){
+                    row = $("<row>").addClass('row')
+                }
+                row.append(b)
+                container.append(row)
 
-
-
+            }
+             var search = $("#container");
+             var recipeTitle = $('<p>' + recipe[i].title + '</p>');
+             var image = $('<img>');
+             image.addClass('pics')
+             image.attr('src', recipe[i].image);
+             image.width(286)
+             image.height(160)
+             search.append(image)
+             search.append(recipeTitle)
+             search.append(container)
+             $('#container').append(search);
+        }
+}) 
